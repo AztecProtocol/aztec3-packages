1828c571aa2b6f40c01f993565879f730683c261
Mitch <mitchell@aztecprotocol.com>
2025-01-27 21:38:12 -0500

    fix compilation

--- yarn-project/end-to-end/src/e2e_p2p/upgrade_governance_proposer.test.ts
+++ yarn-project/end-to-end/src/e2e_p2p/upgrade_governance_proposer.test.ts
@@ -1,6 +1,7 @@
 import { type AztecNodeService } from '@aztec/aztec-node';
 import { deployL1Contract, sleep } from '@aztec/aztec.js';
 import {
+  TestERC20Abi as FeeJuiceAbi,
   GovernanceAbi,
   GovernanceProposerAbi,
   NewGovernanceProposerPayloadAbi,
